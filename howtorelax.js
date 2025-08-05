1d175e3694c27ee0f2945c0038dd207d16df4507
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-08-05 10:55:51 -0700

    Create howtorelax.js

---
+++ howtorelax.js
@@ -0,0 +1,9 @@
+// DESCRIPTION
+document.getElementById("howtorelaxdescription").innerHTML =
+  "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"How to Relax, by Zen teacher Thich Nhat Hanh, introduces beginners and reminds seasoned " + 
+  "practitioners of the essentials of mindfulness practice. A pocket-sized book, How to Relax shows " + 
+  "how critical it is to regularly interrupt the hub-bub and routine of our lives to stop, relax " +
+  "mindfully, and recharge. . . .<br>" +
+  "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"With sections on healing, relief from nonstop thinking, transforming unpleasant sounds, solitude, " +
+  "being peace, and more, How to Relax includes meditations you can do to help you achieve the " +
+  "benefits of relaxation no matter where you are.\"";
